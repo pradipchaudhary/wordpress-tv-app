d9765da64d080bce6b04904c0528cf696b9ab7db
Pradip Chaudhary <chaudharypradip678@gmail.com>
2022-09-16 16:43:19 +0545

    initial commit

---
+++ wp-content/themes/tv-app/footer.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package tv-app
+ */
+
+?>
+
+<footer class="site-footer">
+	<marquee behavior="" direction=""> lates News here..... </marquee>
+</footer><!-- #colophon -->
+
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
---
+++ wp-content/themes/tv-app/front-page.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package tv-app
+ */
+
+get_header();
+?>
+
+<div> Main Content </div>
+
+
+<?php
+get_footer();
